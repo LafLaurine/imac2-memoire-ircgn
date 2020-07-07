13faf83c5d9e67ebb4b2f8cac4599d639e539cb2
Laurine <laurine.lafontaine@outlook.fr>
2020-07-07 17:33:30 +0200

    Fix : new data.csv

--- lib/facealignment/detect_landmarks_in_image.py
+++ lib/facealignment/detect_landmarks_in_image.py
@@ -38,7 +38,7 @@ def main():
     listofrowtodelete = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,31,32,33,34,35,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69])
     imagePoints[0] = np.delete(imagePoints[0],listofrowtodelete,0)
     # We need to find matrice for mean landmarks
-    # savetxt('data.csv', imagePoints[0], delimiter=',')
+    #savetxt('data.csv', imagePoints[0], delimiter=',')
     A = np.asmatrix(loadtxt('data.csv', delimiter=','))
     B = np.asmatrix((imagePoints[0]))
     # trans = np.dot(np.linalg.inv(A),B)

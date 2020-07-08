80b034c6ad5bfca6ad09fe888c9b7a3a0414e3c9
Laurine <laurine.lafontaine@outlook.fr>
2020-07-08 11:07:15 +0200

    Externalize get_rotation_matrix function

---
+++ lib/facealignment/get_rotation_matrix.py
@@ -0,0 +1,55 @@
+from numpy import *
+import numpy as np
+import cv2
+
+def get_rotation_matrix(imagePoints):
+    # row to delete
+    listofrowtodelete = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,31,32,33,34,35,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69])
+    imagePoints[0] = np.delete(imagePoints[0],listofrowtodelete,0)
+
+    #savetxt('data.csv', imagePoints[0], delimiter=',')
+    A = np.asmatrix(loadtxt('data.csv', delimiter=','))
+    B = np.asmatrix((imagePoints[0]))
+    
+    # create column and row to add 
+    columnofones = np.ones((18,1), dtype=int64)
+    rowtoaddtoM = [0,0,0,1]
+
+    # we need to find the refined matrix that transform best A to B
+    A = np.append(A,columnofones,1)
+
+    # we extract the x,y and z from matrix B
+    vector_xp = B[:,0]
+    vector_yp = B[:,1]
+    vector_zp = B[:,2]
+
+    # we solve the overdetermined system
+    M1 = np.linalg.pinv(A).dot(vector_xp)
+    M2 = np.linalg.pinv(A).dot(vector_yp)
+    M3 = np.linalg.pinv(A).dot(vector_zp)
+
+    # we construct matrix M
+    M = np.zeros((4,4))
+    M[0,:] = M1.transpose()
+    M[1,:] = M2.transpose()
+    M[2,:] = M3.transpose()
+    M[3,:] = rowtoaddtoM
+    
+    #test with estimateAffine3D
+    src_point = loadtxt('data.csv', delimiter=',')
+    dst_point = imagePoints[0] 
+    (retval ,E,inliers)= cv2.estimateAffine3D(src_point,dst_point)
+    E = np.delete(E,3,1)
+    print("with estimateAffine3D" , E)
+    # we extract matrix N
+    N = np.delete(M,3,0)
+    N = np.delete(N,3,1)
+    print("with our method" ,N)
+
+    print("the difference" , E-N)
+    
+    # QR decomposition
+    Q, R = np.linalg.qr(N)
+    print(Q)
+    print(R)
+    return Q

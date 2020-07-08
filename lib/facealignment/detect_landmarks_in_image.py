3a30f65a0aa73e232554923720e3e8c9a7f14170
Laurine <laurine.lafontaine@outlook.fr>
2020-07-08 10:52:56 +0200

    Clean : clean test

--- lib/facealignment/detect_landmarks_in_image.py
+++ lib/facealignment/detect_landmarks_in_image.py
@@ -18,7 +18,23 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def get_rotation_matrix():
+
+def main():
+    # Run the 3D face alignment with CPU on a test image : change cpu to cuda
+    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cpu', flip_input=False, face_detector='sfd')
+
+    frame = cv2.imread(image_path)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    # 2D-Plot
+    plot_style = dict(marker='o',
+                    markersize=4,
+                    linestyle='-',
+                    lw=2)
+
+    # Get landmarks of the input image
+    imagePoints = fa.get_landmarks_from_image(frame)
+
     # row to delete
     listofrowtodelete = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,31,32,33,34,35,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69])
     imagePoints[0] = np.delete(imagePoints[0],listofrowtodelete,0)
@@ -62,38 +78,21 @@ def get_rotation_matrix():
     print("with our method" ,N)
 
     print("the difference" , E-N)
+    
     # QR decomposition
     Q, R = np.linalg.qr(N)
     print(Q)
     print(R)
-    return Q
-
-def main():
-    # Run the 3D face alignment with CPU on a test image : change cpu to cuda
-    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cpu', flip_input=False, face_detector='sfd')
-
-    frame = cv2.imread(image_path)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    
-    # 2D-Plot
-    plot_style = dict(marker='o',
-                    markersize=4,
-                    linestyle='-',
-                    lw=2)
-
-    # Get landmarks of the input image
-    imagePoints = fa.get_landmarks_from_image(frame)
+    # we have the rotation matrix
+    x_axis = Q[:,0]
+    y_axis = Q[:,1]
+    z_axis = Q[:,2]
 
     if(imagePoints is not None):
         # Get the array
         imagePoints = imagePoints[0]
         # Compute the Mean-Centered-Scaled Points
         mean = np.mean(imagePoints, axis=0)
-            
-        # we have the rotation matrix
-        x_axis = Q[:,0]
-        y_axis = Q[:,1]
-        z_axis = Q[:,2]
 
         # Draw the computed data
         for imagePoint in imagePoints:
@@ -141,7 +140,6 @@ def main():
                 imagePoints[pred_type.slice, 1],
                 imagePoints[pred_type.slice, 2], color='blue')'''
 
-    
     ax.view_init(elev=90., azim=90.)
     ax.set_xlim(ax.get_xlim()[::-1])
     plt.show()

ae97b72f90f64edb19df3133764fadfe75d89897
Laurine <laurine.lafontaine@outlook.fr>
2020-07-08 09:23:21 +0200

    Add : Remove unused file and draw axes

--- lib/facealignment/detect_landmarks_in_image.py
+++ lib/facealignment/detect_landmarks_in_image.py
@@ -34,50 +34,74 @@ def main():
     # Get landmarks of the input image
     imagePoints = fa.get_landmarks_from_image(frame)
 
-    #row to delete
+    # row to delete
     listofrowtodelete = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,31,32,33,34,35,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69])
     imagePoints[0] = np.delete(imagePoints[0],listofrowtodelete,0)
-    # We need to find matrice for mean landmarks
+
     #savetxt('data.csv', imagePoints[0], delimiter=',')
     A = np.asmatrix(loadtxt('data.csv', delimiter=','))
     B = np.asmatrix((imagePoints[0]))
-    # trans = np.dot(np.linalg.inv(A),B)
     
+    # create column and row to add 
     columnofones = np.ones((18,1), dtype=int64)
     rowtoaddtoM = [0,0,0,1]
+
+    # we need to find the refined matrix that transform best A to B
     A = np.append(A,columnofones,1)
 
+    # we extract the x,y and z from matrix B
     vector_xp = B[:,0]
     vector_yp = B[:,1]
     vector_zp = B[:,2]
 
+    # we solve the overdetermined system
     M1 = np.linalg.pinv(A).dot(vector_xp)
     M2 = np.linalg.pinv(A).dot(vector_yp)
     M3 = np.linalg.pinv(A).dot(vector_zp)
+
+    # we construct matrix M
     M = np.zeros((4,4))
     M[0,:] = M1.transpose()
     M[1,:] = M2.transpose()
     M[2,:] = M3.transpose()
     M[3,:] = rowtoaddtoM
+    # we extract matrix N
     N = np.delete(M,3,0)
     N = np.delete(N,3,1)
     print(N)
+    # QR decomposition
     Q, R = np.linalg.qr(N)
     print(Q)
     print(R)
+    # we have the rotation matrix
+    x_axis = Q[:,0]
+    y_axis = Q[:,1]
+    z_axis = Q[:,2]
 
     if(imagePoints is not None):
         # Get the array
         imagePoints = imagePoints[0]
+        # Compute the Mean-Centered-Scaled Points
+        mean = np.mean(imagePoints, axis=0)
 
         # Draw the computed data
         for imagePoint in imagePoints:
             # Draw the Point Predictions
             cv2.circle(frame, (imagePoint[0], imagePoint[1]), 3, (0,255,0))
 
+    # x_axis
+    cv2.line(frame, tuple(mean[:2].astype(int)), 
+                        tuple((mean+(x_axis * 100.0))[:2].astype(int)), (0, 0, 255), 3)
+
+    # y_axis                   
+    cv2.line(frame, tuple(mean[:2].astype(int)), 
+                        tuple((mean-(y_axis * 100.0))[:2].astype(int)), (0, 255, 0), 3)
+    # z_axis                   
+    cv2.line(frame, tuple(mean[:2].astype(int)), 
+                        tuple((mean+(z_axis * 100.0))[:2].astype(int)), (255, 0, 0), 3)
+
     pred_type = collections.namedtuple('prediction_type', ['slice', 'color'])
-    pred_types = {'face': pred_type(slice(0, 15), (0.682, 0.780, 0.909, 0.5)),
-                }
+    pred_types = {'face': pred_type(slice(0, 15), (0.682, 0.780, 0.909, 0.5))}
     fig = plt.figure(figsize=plt.figaspect(.5))
     ax = fig.add_subplot(1, 2, 1)
     ax.imshow(frame)

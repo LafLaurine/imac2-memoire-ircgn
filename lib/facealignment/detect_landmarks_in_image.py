324c2ffd93cd7e48a60f1e7af16b2746fa3bbbf8
Laurine <laurine.lafontaine@outlook.fr>
2020-07-08 10:48:59 +0200

    Auto stash before merge of "master" and "origin/master"

--- lib/facealignment/detect_landmarks_in_image.py
+++ lib/facealignment/detect_landmarks_in_image.py
@@ -18,22 +18,7 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def main():
-    # Run the 3D face alignment with CPU on a test image : change cpu to cuda
-    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cpu', flip_input=False, face_detector='sfd')
-
-    frame = cv2.imread(image_path)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    
-    # 2D-Plot
-    plot_style = dict(marker='o',
-                    markersize=4,
-                    linestyle='-',
-                    lw=2)
-
-    # Get landmarks of the input image
-    imagePoints = fa.get_landmarks_from_image(frame)
-
+def get_rotation_matrix():
     # row to delete
     listofrowtodelete = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,31,32,33,34,35,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69])
     imagePoints[0] = np.delete(imagePoints[0],listofrowtodelete,0)
@@ -81,16 +66,34 @@ def main():
     Q, R = np.linalg.qr(N)
     print(Q)
     print(R)
-    # we have the rotation matrix
-    x_axis = Q[:,0]
-    y_axis = Q[:,1]
-    z_axis = Q[:,2]
+    return Q
+
+def main():
+    # Run the 3D face alignment with CPU on a test image : change cpu to cuda
+    fa = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cpu', flip_input=False, face_detector='sfd')
+
+    frame = cv2.imread(image_path)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    # 2D-Plot
+    plot_style = dict(marker='o',
+                    markersize=4,
+                    linestyle='-',
+                    lw=2)
+
+    # Get landmarks of the input image
+    imagePoints = fa.get_landmarks_from_image(frame)
 
     if(imagePoints is not None):
         # Get the array
         imagePoints = imagePoints[0]
         # Compute the Mean-Centered-Scaled Points
         mean = np.mean(imagePoints, axis=0)
+            
+        # we have the rotation matrix
+        x_axis = Q[:,0]
+        y_axis = Q[:,1]
+        z_axis = Q[:,2]
 
         # Draw the computed data
         for imagePoint in imagePoints:
@@ -119,10 +122,8 @@ def main():
     ax.imshow(frame)
 
     # that's the part where we draw lines
-    for pred_type in pred_types.values():
-        ax.plot(imagePoints[pred_type.slice, 0],
-                imagePoints[pred_type.slice, 1],
-                color=pred_type.color, **plot_style)
+    '''for pred_type in pred_types.values():
+        ax.plot(imagePoints[pred_type.slice, 0],imagePoints[pred_type.slice, 1],color=pred_type.color, **plot_style)'''
 
     ax.axis('off')
 
@@ -135,17 +136,15 @@ def main():
                     alpha=1.0,
                     edgecolor='b')
 
-    for pred_type in pred_types.values():
+    '''for pred_type in pred_types.values():
         ax.plot3D(imagePoints[pred_type.slice, 0] * 1.2,
                 imagePoints[pred_type.slice, 1],
-                imagePoints[pred_type.slice, 2], color='blue')
+                imagePoints[pred_type.slice, 2], color='blue')'''
 
     
     ax.view_init(elev=90., azim=90.)
     ax.set_xlim(ax.get_xlim()[::-1])
     plt.show()
-    
-
         
 if __name__ == '__main__':
     args = parse_args()

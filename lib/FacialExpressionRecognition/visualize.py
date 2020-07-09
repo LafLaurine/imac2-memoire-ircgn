c4112db72ad90eacf400de0d0cfdaba406935186
Laurine <laurine.lafontaine@outlook.fr>
2020-07-09 10:09:14 +0200

    Clean : reorganize files

--- lib/FacialExpressionRecognition/visualize.py
+++ lib/FacialExpressionRecognition/visualize.py
@@ -24,17 +24,44 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-cut_size = 44
-
-transform_test = transforms.Compose([
-    transforms.TenCrop(cut_size),
-    transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
-])
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
+
+def draw(raw_img,class_names,score):    
+    plt.rcParams['figure.figsize'] = (13.5,5.5)
+    axes=plt.subplot(1, 3, 1)
+    plt.imshow(raw_img)
+    plt.xlabel('Input Image', fontsize=16)
+    axes.set_xticks([])
+    axes.set_yticks([])
+    plt.tight_layout()
+
+    plt.subplots_adjust(left=0.2, bottom=0.2, right=0.95, top=0.9, hspace=0.02, wspace=0.3)
+
+    plt.subplot(1, 3, 2)
+    ind = 0.1+0.6*np.arange(len(class_names))    # the x locations for the groups
+    width = 0.4       # the width of the bars: can also be len(x) sequence
+    color_list = ['red','orangered','darkorange','limegreen','darkgreen','royalblue','navy']
+    for i in range(len(class_names)):
+        plt.bar(ind[i], score.data.cpu().numpy()[i], width, color=color_list[i])
+    plt.title("Classification results ",fontsize=20)
+    plt.xlabel(" Expression Category ",fontsize=16)
+    plt.ylabel(" Classification Score ",fontsize=16)
+    plt.xticks(ind, class_names, rotation=45, fontsize=14)
+
+    plt.savefig(os.path.join(output_path))
+    plt.close()
+
 def main():
+    cut_size = 44
+
+    transform_test = transforms.Compose([
+        transforms.TenCrop(cut_size),
+        transforms.Lambda(lambda crops: torch.stack([transforms.ToTensor()(crop) for crop in crops])),
+    ])
+
     raw_img = cv2.imread(image_path)[...,::-1]
     gray = rgb2gray(raw_img)
     gray = cv2.resize(gray, (48,48)).astype(np.uint8)
@@ -64,31 +91,7 @@ def main():
 
     score = F.softmax(outputs_avg,dim=0)
     _, predicted = torch.max(outputs_avg.data, 0)
-
-    plt.rcParams['figure.figsize'] = (13.5,5.5)
-    axes=plt.subplot(1, 3, 1)
-    plt.imshow(raw_img)
-    plt.xlabel('Input Image', fontsize=16)
-    axes.set_xticks([])
-    axes.set_yticks([])
-    plt.tight_layout()
-
-    plt.subplots_adjust(left=0.2, bottom=0.2, right=0.95, top=0.9, hspace=0.02, wspace=0.3)
-
-    plt.subplot(1, 3, 2)
-    ind = 0.1+0.6*np.arange(len(class_names))    # the x locations for the groups
-    width = 0.4       # the width of the bars: can also be len(x) sequence
-    color_list = ['red','orangered','darkorange','limegreen','darkgreen','royalblue','navy']
-    for i in range(len(class_names)):
-        plt.bar(ind[i], score.data.cpu().numpy()[i], width, color=color_list[i])
-    plt.title("Classification results ",fontsize=20)
-    plt.xlabel(" Expression Category ",fontsize=16)
-    plt.ylabel(" Classification Score ",fontsize=16)
-    plt.xticks(ind, class_names, rotation=45, fontsize=14)
-
-    plt.savefig(os.path.join(output_path))
-    plt.close()
-
+    draw(raw_img,class_names,score)
     print("The Expression is %s" %str(class_names[int(predicted.cpu().numpy())]))
 
 if __name__ == '__main__':

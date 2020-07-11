1a529a7551d67d5d17f260b8ea1271977c0e939e
Laurine <laurine.lafontaine@outlook.fr>
2020-07-11 14:31:52 +0200

    Add : ConvolutionalAutoEncoder

--- src/decoder_test/keras_test.py
+++ src/decoder_test/keras_test.py
@@ -1,10 +1,31 @@
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
 from keras.models import Model
+from keras import backend as K
 from keras.datasets import mnist
+from keras import regularizers
 import numpy as np
 import matplotlib.pyplot as plt
 
-class FullyConnected:
+def draw(x_test,decoded_imgs):
+    n = 10
+    plt.figure(figsize=(20, 4))
+    for i in range(n):
+        # display original
+        ax = plt.subplot(2, n, i+1)
+        plt.imshow(x_test[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # display reconstruction
+        ax = plt.subplot(2, n, i + n)
+        plt.imshow(decoded_imgs[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    plt.show()
+
+class FullyConnectedEncoder:
     def main():
         # this is the size of our encoded representations
         encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
@@ -12,7 +33,7 @@ class FullyConnected:
         # this is our input placeholder
         input_img = Input(shape=(784,))
         # "encoded" is the encoded representation of the input
-        encoded = Dense(encoding_dim, activation='relu')(input_img)
+        encoded = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.l1(10e-5))(input_img)
         # "decoded" is the lossy reconstruction of the input
         decoded = Dense(784, activation='sigmoid')(encoded)
 
@@ -47,23 +68,45 @@ class FullyConnected:
         # encode and decode some digits
         encoded_imgs = encoder.predict(x_test)
         decoded_imgs = decoder.predict(encoded_imgs)
+        draw(x_test,decoded_imgs)
 
-        n = 10  # how many digits we will display
-        plt.figure(figsize=(20, 4))
-        for i in range(n):
-            # display original
-            ax = plt.subplot(2, n, i + 1)
-            plt.imshow(x_test[i].reshape(28, 28))
-            plt.gray()
-            ax.get_xaxis().set_visible(False)
-            ax.get_yaxis().set_visible(False)
+class ConvolutionalAutoEncoder():
 
-            # display reconstruction
-            ax = plt.subplot(2, n, i + 1 + n)
-            plt.imshow(decoded_imgs[i].reshape(28, 28))
-            plt.gray()
-            ax.get_xaxis().set_visible(False)
-            ax.get_yaxis().set_visible(False)
-        plt.show()
+    def main():
+        input_img = Input(shape=(28, 28, 1))
+        x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+        x = MaxPooling2D((2, 2), padding='same')(x)
+        x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+        encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-FullyConnected.main()
+        # at this point the representation is (4, 4, 8) i.e 128-dimensional
+
+        x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2D(16, (3, 3), activation='relu')(x)
+        x = UpSampling2D((2, 2))(x)
+        decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+        autoencoder = Model(input_img, decoded)
+        autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+        (x_train, _), (x_test, _) = mnist.load_data()
+
+        x_train = x_train.astype('float32') / 255.
+        x_test = x_test.astype('float32') / 255.
+        x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+        x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+        autoencoder.fit(x_train, x_train,
+                    epochs=3,
+                    batch_size=128,
+                    shuffle=True,
+                    validation_data=(x_test, x_test))
+        
+        decoded_imgs = autoencoder.predict(x_test)
+        draw(x_test,decoded_imgs)
+
+ConvolutionalAutoEncoder.main()

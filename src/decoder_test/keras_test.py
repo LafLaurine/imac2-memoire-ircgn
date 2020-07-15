582fccda6e8e7f55dc8c385782f9102473a316f3
Laurine <laurine.lafontaine@outlook.fr>
2020-07-15 09:35:30 +0200

    Add : LSTM autoencoder

--- src/decoder_test/keras_test.py
+++ src/decoder_test/keras_test.py
@@ -1,11 +1,16 @@
-from keras.layers import Input, Dense, Lambda, Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Dense, Lambda, Conv2D, MaxPooling2D, UpSampling2D, LSTM
 from keras.models import Model
 from keras import backend as K
 from keras.datasets import mnist
 from keras.losses import mse, binary_crossentropy
 from keras import regularizers
 from keras.utils import plot_model
+from keras.utils import to_categorical
+from random import randint
 import numpy as np
+from numpy import array
+from numpy import argmax
+from numpy import array_equal
 import matplotlib.pyplot as plt
 import argparse
 import os
@@ -192,11 +197,101 @@ class VAE:
     def vae_loss(x, x_decoded_mean):
         xent_loss = objectives.binary_crossentropy(x, x_decoded_mean)
         kl_loss = - 0.5 * K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-        return xent_loss + kl_loss      
+        return xent_loss + kl_loss
+
+
+class LSTMAutoEncoder:
+    # generate a sequence of random integers
+    def generate_sequence(length, n_unique):
+        return [randint(1, n_unique-1) for _ in range(length)]
+
+    # prepare data for the LSTM
+    def get_dataset(n_in, n_out, cardinality, n_samples):
+        X1, X2, y = list(), list(), list()
+        for _ in range(n_samples):
+                # generate source sequence
+                source = LSTMAutoEncoder.generate_sequence(n_in, cardinality)
+                # define padded target sequence
+                target = source[:n_out]
+                target.reverse()
+                # create padded input target sequence
+                target_in = [0] + target[:-1]
+                # encode
+                src_encoded = to_categorical([source], num_classes=cardinality)
+                tar_encoded = to_categorical([target], num_classes=cardinality)
+                tar2_encoded = to_categorical([target_in], num_classes=cardinality)
+                # store
+                X1.append(src_encoded)
+                X2.append(tar2_encoded)
+                y.append(tar_encoded)
+        X1 = np.squeeze(array(X1), axis=1) 
+        X2 = np.squeeze(array(X2), axis=1) 
+        y = np.squeeze(array(y), axis=1) 
+        return X1, X2, y
+
+    # returns train, inference_encoder and inference_decoder models
+    def define_models(n_input, n_output, n_units):
+        # define training encoder
+        encoder_inputs = Input(shape=(None, n_input))
+        encoder = LSTM(n_units, return_state=True)
+        encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+        encoder_states = [state_h, state_c]
+        # define training decoder
+        decoder_inputs = Input(shape=(None, n_output))
+        decoder_lstm = LSTM(n_units, return_sequences=True, return_state=True)
+        decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+        decoder_dense = Dense(n_output, activation='softmax')
+        decoder_outputs = decoder_dense(decoder_outputs)
+        model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+        # define inference encoder
+        encoder_model = Model(encoder_inputs, encoder_states)
+        # define inference decoder
+        decoder_state_input_h = Input(shape=(n_units,))
+        decoder_state_input_c = Input(shape=(n_units,))
+        decoder_states_inputs = [decoder_state_input_h, decoder_state_input_c]
+        decoder_outputs, state_h, state_c = decoder_lstm(decoder_inputs, initial_state=decoder_states_inputs)
+        decoder_states = [state_h, state_c]
+        decoder_outputs = decoder_dense(decoder_outputs)
+        decoder_model = Model([decoder_inputs] + decoder_states_inputs, [decoder_outputs] + decoder_states)
+        # return all models
+        return model, encoder_model, decoder_model
+
+    # generate target given source sequence
+    def predict_sequence(infenc, infdec, source, n_steps, cardinality):
+        # encode
+        state = infenc.predict(source)
+        # start of sequence input
+        target_seq = array([0.0 for _ in range(cardinality)]).reshape(1, 1, cardinality)
+        # collect predictions
+        output = list()
+        for t in range(n_steps):
+            # predict next char
+            yhat, h, c = infdec.predict([target_seq] + state)
+            # store prediction
+            output.append(yhat[0,0,:])
+            # update state
+            state = [h, c]
+            # update target sequence
+            target_seq = yhat
+        return array(output)
+
+    def main(): 
+        # configure problem
+        n_features = 50 + 1
+        n_steps_in = 6
+        n_steps_out = 3
+        # define model
+        train, infenc, infdec = LSTMAutoEncoder.define_models(n_features, n_features, 128)
+        train.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        # generate training dataset
+        X1, X2, y = LSTMAutoEncoder.get_dataset(n_steps_in, n_steps_out, n_features, 100000)
+        print(X1.shape,X2.shape,y.shape)
+        # train model
+        train.fit([X1, X2], y, epochs=1)
 
 if __name__ == '__main__':
     # MNIST dataset
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    '''(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     image_size = x_train.shape[1]
     original_dim = image_size * image_size
@@ -279,4 +374,6 @@ if __name__ == '__main__':
     VAE.plot_results(models,
                  data,
                  batch_size=batch_size,
-                 model_name="vae_mlp")
+                 model_name="vae_mlp")'''
+
+    LSTMAutoEncoder.main()

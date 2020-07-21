8e03463ce8b7e03212c9640641906e84c8cdcc5e
Laurine <laurine.lafontaine@outlook.fr>
2020-07-21 15:43:49 +0200

    Add : Test on GAN

--- src/decoder_test/keras_test.py
+++ src/decoder_test/keras_test.py
@@ -34,50 +34,6 @@ def draw(x_test,decoded_imgs):
         ax.get_yaxis().set_visible(False)
     plt.show()
 
-class FullyConnectedEncoder:
-    def main():
-        # this is the size of our encoded representations
-        encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
-
-        # this is our input placeholder
-        input_img = Input(shape=(784,))
-        # "encoded" is the encoded representation of the input
-        encoded = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.l1(10e-5))(input_img)
-        # "decoded" is the lossy reconstruction of the input
-        decoded = Dense(784, activation='sigmoid')(encoded)
-
-        # this model maps an input to its reconstruction
-        autoencoder = Model(input_img, decoded)
-
-        # this model maps an input to its encoded representation
-        encoder = Model(input_img, encoded)
-        # create a placeholder for an encoded (32-dimensional) input
-        encoded_input = Input(shape=(encoding_dim,))
-        # retrieve the last layer of the autoencoder model
-        decoder_layer = autoencoder.layers[-1]
-        # create the decoder model
-        decoder = Model(encoded_input, decoder_layer(encoded_input))
-        autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
-        # input data
-        (x_train, _), (x_test, _) = mnist.load_data()
-        # normalize all values between 0 and 1 : flatten 28x28 images into vectors of size 784
-        x_train = x_train.astype('float32') / 255.
-        x_test = x_test.astype('float32') / 255.
-        x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-        x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-        print(x_train.shape)
-        print(x_test.shape)
-
-        # train autoencoder
-        autoencoder.fit(x_train, x_train,
-                epochs=10,
-                batch_size=256,
-                shuffle=True,
-                validation_data=(x_test, x_test))
-        # encode and decode some digits
-        encoded_imgs = encoder.predict(x_test)
-        decoded_imgs = decoder.predict(encoded_imgs)
-        draw(x_test,decoded_imgs)
 
 class ConvolutionalAutoEncoder:
     def main():

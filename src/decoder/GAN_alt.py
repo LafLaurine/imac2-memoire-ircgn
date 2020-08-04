5fbec073ceed4937112509fc1800f5162577e141
Pierre Labendzki <pierre.labendzki@gmail.com>
2020-08-04 10:33:23 +0200

    automatic csv

--- src/decoder/GAN_alt.py
+++ src/decoder/GAN_alt.py
@@ -67,7 +67,7 @@ def build_generator(start_filters, filter_size, latent_dim):
   return Model(inputs=inp, outputs=x)
 
 # load our dataset attributes
-df_celeb = pd.read_csv('../data.csv')
+df_celeb = pd.read_csv('../all_data.csv')
 TOTAL_SAMPLES = df_celeb.shape[0]
 
 # we will downscale the images
--- src/from_txt_to_csv.py
+++ src/from_txt_to_csv.py
@@ -3,21 +3,14 @@ import numpy as np
 import os
 import collections
 import sys
-
 col_lips = np.float32(np.loadtxt('lips_dist.txt', delimiter=','))
 col_angles = np.float32(np.loadtxt('euler_angles.txt'))
 col_expression = np.float32(np.loadtxt('expression.txt'))
+lenth = len(col_lips)
 
-
-with open("data.csv", 'w') as outfile:  
-    # creating a csv writer object  
+with open("all_data.csv", 'w') as outfile:
     csvwriter = csv.writer(outfile)
     csvwriter.writerow(['File_name','Lips_distance', 'Euler\'s_angles','Expression'])
-    csvwriter.writerows([['000001.jpg']])
-    csvwriter.writerows([['000002.jpg']])
-    csvwriter.writerows([['000003.jpg']])
-    csvwriter.writerows([['000004.jpg']])
-    csvwriter.writerows([['000005.jpg']])
-    csvwriter.writerows([col_lips])
-    csvwriter.writerows([col_angles])
-    csvwriter.writerows([col_expression])
+    for i in range(lenth):
+        # Add the data row
+        csvwriter.writerow(['00000'+str(i+1)+'.jpg',col_lips[i] ,col_angles[i] ,[col_expression[7*i],col_expression[7*i+1],col_expression[7*i+2],col_expression[7*i+3],col_expression[7*i+4],col_expression[7*i+5],col_expression[7*i+6]]])
